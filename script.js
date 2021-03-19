4760ee9e8214033153d78291179789b835d8f007
danielaucarpro <76978089+danielaucarpro@users.noreply.github.com>
2021-03-19 12:35:51 -0300

    LAST UPDATE

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+// Calculator
 //variables
 var result = document.getElementById('result');
- 
-//on document ready
-$(document).ready(result.value = 0)
+var plusMinus = document.getElementById('plusMinus');
+
+$(document).ready(() => {
+    let operator = document.querySelectorAll('#operator');
+    //for each operator class I have in my html, make the button disabled.
+    for (i = 0; i < operator.length; i++) {
+        operator[i].disabled = true;
+    }
+})
 
 //Clearing the screen
 function clearScreen() {
     //when the button id clicked set the value of the result element to blank
-    result.value = 0;
+    result.value = '';
 }
 
 //Change the result value according with number clicked
@@ -34,43 +41,25 @@ function sendOperator(value) {
 
 //do basic operations doing eval
 function evalOp() {
-    // storing the input with result id
-    let result = document.getElementById('result');
-
     //the result value now will be the result of a basic operation done by eval
     //eval gets the value inside input and make a operation according with what user typed
     result.value = eval(result.value);
 }
 
 //changing the number to positive and negative value. Using true or false method
-//on click add a plus o minus value in front of the number
-//if there is a operator already do nothing
+//on click add a minus value in front of the number
+//if clicked again remove 
 function plusMinus() {
     result.value = '-' + result.value;
-    let count = 0;
-    count++;
-    console.log(count);
-}
-
-function removePlusMinus(bool){
-    if(bool = true){
-        result.value = "" + result.value;
-        iveBeenPressed = false;
-        console.log(iveBeenPressed);
-    }
 }
 
 //converting the number to percentage format
 function percentage() {
-    var result = document.getElementById('result');
-    if (result.value === null) {
-        document.getElementById('result').value = "";
-    }
-    else if (result.value != 0) {
+    if (result.value != 0) {
         result.value = eval(result.value);
         let percentage = result.value / 100;
         console.log(percentage);
         result.value = '';
-        sendOperator(percentage);
+        sendNumber(percentage);
     }
 }
